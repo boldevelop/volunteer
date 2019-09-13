27b2e8bea028c4513dcd3b700b4e50e25d77e84e
Владислав Болдаков <15webboldakov@gmail.com>
2019-09-13 22:51:30 +0700

    Updates

---
+++ precache-manifest.bf86957c10c115743a4803630eb8f7da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41662e4f36393f3f41e1",
+    "url": "/volunteer/static/js/runtime~main.bf2fe941.js"
+  },
+  {
+    "revision": "bb270197f257327a5184",
+    "url": "/volunteer/static/js/main.585e0154.chunk.js"
+  },
+  {
+    "revision": "e03ae65e3e06bf0b9912",
+    "url": "/volunteer/static/js/2.192e86e0.chunk.js"
+  },
+  {
+    "revision": "bb270197f257327a5184",
+    "url": "/volunteer/static/css/main.ee4f72aa.chunk.css"
+  },
+  {
+    "revision": "e03ae65e3e06bf0b9912",
+    "url": "/volunteer/static/css/2.d17dc77b.chunk.css"
+  },
+  {
+    "revision": "eb138afdcfec16bcb8c8ce28e9c8d79e",
+    "url": "/volunteer/index.html"
+  }
+];
